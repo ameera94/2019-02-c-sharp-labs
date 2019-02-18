f023c3199f41d1c7123a045c0589ca257e5460c5
ameera94 <ashah2@spartaglobal.com>
2019-02-18 17:07:20 +0000

    speed type added

---
+++ labs/Labs_Test/LabsTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using lab_112_collections;
+
+
+namespace Labs_Test
+{
+    [TestClass]
+    public class LabsTest
+    {
+        [TestMethod]
+        public void Lab112CollectionsTest1()
+        {
+            //ARRANGE
+            var expected01 = 22400;            
+            var instanceLab112Collection = new Collections();
+
+
+            //ACT
+            var actual01 = instanceLab112Collection.Collections20MinuteLab(10, 20, 30);
+           
+
+            //ASSERT
+            Assert.AreEqual(expected01, actual01);
+
+        }
+
+        [TestMethod]
+        public void Lab112CollectionsTest2()
+        {
+            //ARRANGE
+            var expected02 = 6944;
+            var instanceLab112Collection = new Collections();
+
+            //ACT
+            var actual02 = instanceLab112Collection.Collections20MinuteLab(11, 12, 13);
+
+            //ASSERT
+            Assert.AreEqual(expected02, actual02);
+
+        }
+    }
+}
