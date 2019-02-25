d1b016124dc9bcff667623f0ef1a02a78d596d6d
ameera94 <ashah2@spartaglobal.com>
2019-02-25 10:23:19 +0000

    lab 116

--- labs/lab_116_exceptions/Program.cs
+++ labs/lab_116_exceptions/Program.cs
@@ -10,6 +10,50 @@ namespace lab_116_exceptions
     {
         static void Main(string[] args)
         {
+
+            int x = 10;
+            int y = 0; 
+
+            try
+            {
+                try
+                {
+                    throw new Exception("Phil's special exception");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    Console.WriteLine(e.Data);
+                    Console.WriteLine(e.Message);
+                    throw;
+                }
+                finally
+                {
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\nThey - thrown exception has been caught at an upper level\n");
+            }
+            finally
+            {
+                Console.WriteLine("\nAll Done");
+            }
+
+            try
+            {
+                Console.WriteLine(x / y);
+            }
+            catch(DivideByZeroException) 
+            {
+                Console.WriteLine("\n\n\n\nDivision of {0} by zero.", x);
+            }
+            finally
+            {
+                Console.WriteLine("\nDone");
+            }
         }
     }
 }
+
+
